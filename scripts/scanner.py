7d4b357cbe7ee344b45a7aebaa8e6e37047cee14
HAMZA\hamza <hamzaogretmis@gmail.com>
2026-03-05 20:55:03 +0300

    Perspective Transform has implemented

--- scripts/scanner.py
+++ scripts/scanner.py
@@ -136,15 +136,61 @@ class DocumentScanner:
 
         return None
 
+    # Helper method for perspective transformation gives corner coordinates
+    def _order_points(self, pts):
+        rect = np.zeros((4,2), dtype = "float32")
+
+        # Top left and bottom right corner
+        s = pts.sum(axis=1)
+        rect[0] = pts[np.argmin(s)]
+        rect[2] = pts[np.argmax(s)]
+
+        # Top right and bottom left corner
+        diff = np.diff(pts, axis = 1)
+        rect[1] = pts[np.argmin(diff)]
+        rect[3] = pts[np.argmax(diff)]
+
+        return rect
+
+    # Warping the document to top down perspective
+    def get_scan(self, contour):
+        pts = contour.reshape(4,2) * self.ratio
+        rect = self._order_points(pts)
+        (tl, tr, br, bl) = rect
+
+        widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+        widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+        maxWidth = max(int(widthA), int(widthB))
+
+        heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+        heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+        maxHeight = max(int(heightA), int(heightB))
+
+        dst = np.array([
+            [0,0],
+            [maxWidth - 1, 0],
+            [maxWidth - 1, maxHeight - 1],
+            [0, maxHeight - 1]], dtype = "float32")
+
+        M = cv2.getPerspectiveTransform(rect, dst)
+        warped = cv2.warpPerspective(self.original, M, (maxWidth, maxHeight))
+
+        return warped
+
 # Test Block
 if __name__ == "__main__":
-    scanner = DocumentScanner("../pictures/test.jpg")
+    scanner = DocumentScanner("../pictures/sudoku.png")
     doc_contour = scanner.find_document_contour()
+
     if doc_contour is not None:
         cv2.drawContours(scanner.resized, [doc_contour], -1,
                          (0, 255, 0), 2)
+        cv2.imshow("STEP 1: Detected Document", scanner.resized)
+
+        final_scan = scanner.get_scan(doc_contour)
+        cv2.imshow("STEP 2: Flattened Scan", final_scan)
     else:
         print("Both algorithms failed")
-    cv2.imshow("Document Outline", scanner.resized)
+
     cv2.waitKey(0)
     cv2.destroyAllWindows()
